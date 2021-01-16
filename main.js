07955322e35b74afc7d5fd8f58d63b35567c1061
pancitoConQuesito <gonzalo.astorga.@gmail.com>
2021-01-16 03:57:22 -0300

    p3 primer intento de traslado total

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-let titulo = document.querySelector('.miH1');
-titulo.textContent = 'titulo cambiado';
+ScrollReveal().reveal('.descarga'),{delay:300};
+ScrollReveal().reveal('.productos'),{delay:300};
+ScrollReveal().reveal('.celu'),{delay:300};
+ScrollReveal().reveal('.productos .galeriaImg a img'),{delay:300};
+ScrollReveal().reveal('.quienes_somos .galeriaImg a img'),{delay:300};
+ScrollReveal().reveal('footer a img'),{delay:300};
